18322d09cd8caf1b032cfa916826a6dd9a1f48a7
Maflex24 <maflex24@gmail.com>
2021-10-30 22:18:38 +0200

    Second version, working, but copied files don't keep directory structure

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace BackupLocalFiles_CSharp
@@ -11,6 +12,9 @@ namespace BackupLocalFiles_CSharp
             Console.Clear();
             string sourceBackupPath = Path.Combine(Directory.GetCurrentDirectory(), "sourceCopy");
             string destinationBackupPath = Path.Combine(Directory.GetCurrentDirectory(), "destinationCopy");
+
+            // string sourceBackupPath = $"F:{Path.AltDirectorySeparatorChar}Photo";
+            // string destinationBackupPath = $"F:{Path.AltDirectorySeparatorChar}PhotoCopyOne";
 
             if (Directory.Exists(sourceBackupPath))
             {
@@ -24,14 +28,27 @@ namespace BackupLocalFiles_CSharp
                     System.Console.WriteLine("destination directory not existed, but it was created");
                 }
 
-                string[] files = Directory.GetFiles(sourceBackupPath);
-
-                foreach (string file in files)
+                System.Collections.Generic.IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles(sourceBackupPath, "*", SearchOption.AllDirectories);
+                System.Console.WriteLine("All files in source backup folders:");
+                foreach (var file in allFilesInAllFolders)
                 {
+                    System.Console.WriteLine(file);
+                    System.Console.WriteLine(Directory.GetCurrentDirectory());
+                    System.Console.WriteLine("");
+
                     string sourceFileName = Path.GetFileName(file);
                     string destinationFilePathAndName = Path.Combine(destinationBackupPath, sourceFileName);
                     File.Copy(file, destinationFilePathAndName, true);
                 }
+
+                // string[] files = Directory.GetFiles(sourceBackupPath);
+
+                // foreach (string file in files)
+                // {
+                //     string sourceFileName = Path.GetFileName(file);
+                //     string destinationFilePathAndName = Path.Combine(destinationBackupPath, sourceFileName);
+                //     File.Copy(file, destinationFilePathAndName, true);
+                // }
             }
             else
             {
