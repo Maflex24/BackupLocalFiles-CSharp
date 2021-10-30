2307b7dcfcfef236939d4f78f9d0316b02921efa
Maflex24 <maflex24@gmail.com>
2021-10-30 21:35:35 +0200

    First version, works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace BackupLocalFiles_CSharp
 {
@@ -6,7 +8,35 @@ namespace BackupLocalFiles_CSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            string sourceBackupPath = Path.Combine(Directory.GetCurrentDirectory(), "sourceCopy");
+            string destinationBackupPath = Path.Combine(Directory.GetCurrentDirectory(), "destinationCopy");
+
+            if (Directory.Exists(sourceBackupPath))
+            {
+                if (Directory.Exists(destinationBackupPath))
+                {
+                    System.Console.WriteLine("destination directory exist");
+                }
+                else
+                {
+                    Directory.CreateDirectory(destinationBackupPath);
+                    System.Console.WriteLine("destination directory not existed, but it was created");
+                }
+
+                string[] files = Directory.GetFiles(sourceBackupPath);
+
+                foreach (string file in files)
+                {
+                    string sourceFileName = Path.GetFileName(file);
+                    string destinationFilePathAndName = Path.Combine(destinationBackupPath, sourceFileName);
+                    File.Copy(file, destinationFilePathAndName, true);
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("souce path doesn't exist");
+            }
         }
     }
 }
